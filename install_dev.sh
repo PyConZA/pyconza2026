a363a097a3997cc66a12b540b323e9f727e82b32
Sheena O'Connell <sheena.oconnell@gmail.com>
2026-05-21 07:19:21 -0700

    added createsuperuser to installdev script

--- install_dev.sh
+++ install_dev.sh
@@ -8,6 +8,10 @@ uv run python manage.py migrate
 uv run python manage.py createcachetable wafer_cache_table
 uv run python manage.py wafer_add_default_groups
 
+# create a superuser for local development
+DJANGO_SUPERUSER_PASSWORD='super' uv run python manage.py createsuperuser \
+  --noinput --username super --email super@email.com
+
 # load the markdown content files in `./pages_md`
 uv run python manage.py load_md_content
 
