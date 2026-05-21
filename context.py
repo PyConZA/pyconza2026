e1ccf19dfab3132d3048f5718db57f3095e85746
Sheena O'Connell <sheena.oconnell@gmail.com>
2026-05-21 11:50:08 -0700

    footer area better

--- context.py
+++ context.py
@@ -7,6 +7,7 @@ def context(request):
     return {
         "conference_name": settings.CONFERENCE_NAME,
         "social_links": settings.SOCIAL_LINKS,
+        "FOOTER_MENUS": settings.FOOTER_MENUS,
         "GRANT_APPLICATIONS_OPEN": settings.GRANT_APPLICATIONS_OPEN,
         "VISA_LETTER_REQUESTS_OPEN": settings.VISA_LETTER_REQUESTS_OPEN,
         "CFP_DEADLINE": date.fromisoformat(settings.CFP_DEADLINE),
